a9411e42b9949b6597c4047bf6169b10dea21ed5
kondo tomonori <tomonorik1001@gmail.com>
2022-08-06 09:38:28 +0900

    feat: 下層ページを作成したり、next-head-seoを導入したり色々

---
+++ components/containers/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <>
+      <footer className={styles.footer}>
+        <p>&copy; 2022 Tomonori Kondo </p>
+      </footer>
+    </>
+  )
+}
+
---
+++ pages/privacy.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import styles from '../styles/pages/Privacy.module.css'
+import NextHeadSeo from 'next-head-seo';
+
+const Privacy: NextPage = () => {
+  return (
+    <>
+     <NextHeadSeo
+      title="PrivacyPolicy"
+     />
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Praivacy
+        </h1>
+      </main>
+
+      <footer className={styles.footer}>
+        <p>&copy; 2022 Tomonori Kondo </p>
+      </footer>
+    </>
+  )
+}
+
+export default Privacy
